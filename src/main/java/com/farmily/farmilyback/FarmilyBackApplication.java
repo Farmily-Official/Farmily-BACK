64ccb9d61fde81583201e8599227306319e6d1df
dlrjs2360 <lhg3670@naver.com>
2022-10-09 23:49:53 +0900

    First Commit!

---
+++ src/main/java/com/farmily/farmilyback/FarmilyBackApplication.java
@@ -0,0 +1,13 @@
+package com.farmily.farmilyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmilyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmilyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/farmily/farmilyback/FarmilyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmily.farmilyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmilyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
